b397c4e5d4e10fa0cad2bbe641e6a6f84ed7752d
Aikar <aikar@aikar.co>
2018-03-11 12:48:43 -0400

    Add javadoc/sources to deploy

--- core/src/main/java/co/aikar/db/DatabaseOptions.java
+++ core/src/main/java/co/aikar/db/DatabaseOptions.java
@@ -2,6 +2,7 @@ package co.aikar.db;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NonNull;
 
 import java.util.concurrent.ExecutorService;
@@ -9,17 +10,29 @@ import java.util.function.Consumer;
 import java.util.logging.Logger;
 
 @SuppressWarnings("UnusedAssignment")
-@Builder(toBuilder = true) @AllArgsConstructor
+@Builder(toBuilder = true) @Data
 public class DatabaseOptions {
     private static final DatabaseTiming NULL_TIMING = new NullDatabaseTiming();
+    /**
+     * JDBC DSN to connect to
+     */
     @NonNull String dsn;
+    /**
+     * JDBC Driver name to load
+     */
     @NonNull String databaseClassName;
 
     @Builder.Default String poolName = "DB";
     @Builder.Default String defaultIsolationLevel = "TRANSACTION_READ_COMMITTED";
     @Builder.Default boolean useOptimizations = true;
 
+    /**
+     * For Async queries, minimum threads in the pool to use.
+     */
     @Builder.Default int minAsyncThreads = Math.min(Runtime.getRuntime().availableProcessors(), 2);
+    /**
+     * For Async queries, maximum threads in the pool to use.
+     */
     @Builder.Default int maxAsyncThreads = Runtime.getRuntime().availableProcessors();
     @Builder.Default int asyncThreadTimeout = 60;
     @Builder.Default TimingsProvider timingsProvider = (name, parent) -> NULL_TIMING;
